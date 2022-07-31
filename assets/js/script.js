a124f50162fff2aec617d791e02608e0bf716ee8
Brometheus <zs.serrano1@gmail.com>
2022-07-31 12:55:25 -0500

    initial commits and pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Pseudocode
+
+// Create a Search bar that takes an input and saves it to local storage.
+
+// This input is also saved as a clickable button so the user can go back to 
+// past searches.  All buttons and input can be on left side of page.
+
+// Right side includes two sections: top piece for certain pieces of information
+// in the currently searched city, and bottom piece showing the five day forecast
+// for the same location.
+
+// Top section 'header' will include the city name, date, and icon for the current weather conditions.
+
+// Top section will include current temperature, wind speed, humidity, UV index, and
+// a coloration around the index displaying which of three conditions it falls under.
+
+// Bottom section will include five cards populated with the five day forecast
+
+// These cards will show the future dates, icons for forecast weather conditions, 
+// temperature, wind speed, and humidity.
+
+// Utilize openweather API to gather all of this information and display it properly.
+
