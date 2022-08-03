66b789b59d036736c444cb7ef7441fd2bf998267
Brometheus <zs.serrano1@gmail.com>
2022-08-02 21:08:24 -0500

    work on api calls and current day weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,8 @@
 // for the same location.
 
 // Top section 'header' will include the city name, date, and icon for the current weather conditions.
-$('#searchButton').on('click',function() {
-    locationInput($('').val())
+$('#search').on('click',function() {
+    locationInput($('#inputCity').val())
     // set value to local storage
     // 
 })
@@ -19,7 +19,7 @@ $('#searchButton').on('click',function() {
 function locationInput(entry) {
     var lati;
     var long;
-    // var searchUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=${entry}&limit=1&appid=';
+    var searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${entry}&limit=1&appid=768e5ecb743a4297ff5e9f4b1974e4c3`;
 // need to figure out code to make sure ${entry} is being read properly
     fetch(searchUrl)
     .then(response => response.json())
@@ -31,27 +31,45 @@ function locationInput(entry) {
 }
 
 function weatherForecast(lat, lon) {
-    // var weatherData = 'openweatherapi'
+    var weatherData = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=768e5ecb743a4297ff5e9f4b1974e4c3`;
     fetch(weatherData)
     .then(response => response.json())
     .then(data => {
         var today = $('#currentDay');
         // city name
+        var currentCity = $('#inputCity').val();
         // date
+        var date = new Date(data.current.dt * 1000);
+        var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+        var month = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
+        var year = date.getFullYear();
+        var currentDate = month + '/' + day + '/' + year;
         // weather icon
+        var weatherIcon = data.current.weather.icon;
         // current temp
+        var currentTemp = data.current.temp;
         // humidity
+        var currentHumid = data.current.humidity;
         // wind speed
+        var windSpeed = data.current.wind_speed;
         // uv index
+        var uv = data.current.uvi;
         // uv index is colored based on conditions (favorable, moderate, or severe)
-        var future = $('#forecacst');
-        // create 5 cards. each card will display:
-        // date
-        // weather icon
-        // future average temp
-        // wind speed
-        // humidity
+        today.append(currentCity, currentDate, weatherIcon, currentTemp, currentHumid, windSpeed, uv);
 
     })
+    
+    // var forecastData = `forecastapi`
+    // fetch(forecastData)
+    // .then(response => response.json()
+    // .then(data => {
+    //     var future = $('#forecast');
+    //     // create 5 cards. each card will display:
+    //     // date
+    //     // weather icon
+    //     // future average temp
+    //     // wind speed
+    //     // humidity
+    // }))
 }
 
