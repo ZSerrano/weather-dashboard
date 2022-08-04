dc6332b72df92787b300ea361734c01dc3966075
Brometheus <zs.serrano1@gmail.com>
2022-08-04 18:31:58 -0500

    fixed api key security and switched to one call 2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@
 // Right side includes two sections: top piece for certain pieces of information
 // in the currently searched city, and bottom piece showing the five day forecast
 // for the same location.
-
+var apiKey = '768e5ecb743a4297ff5e9f4b1974e4c3'
 // Top section 'header' will include the city name, date, and icon for the current weather conditions.
 $('#searchButton').on('click',function() {
     locationInput($('#inputCity').val())
@@ -19,7 +19,7 @@ $('#searchButton').on('click',function() {
 function locationInput(entry) {
     var lati;
     var long;
-    var searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${entry}&limit=1&appid=768e5ecb743a4297ff5e9f4b1974e4c3`;
+    var searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${entry}&limit=1&appid=${apiKey}`;
 // need to figure out code to make sure ${entry} is being read properly
     fetch(searchUrl)
     .then(response => response.json())
@@ -31,11 +31,12 @@ function locationInput(entry) {
 }
 
 function weatherForecast(lat, lon) {
-    var apiKey = '768e5ecb743a4297ff5e9f4b1974e4c3'
-    var weatherData = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    
+    var weatherData = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
     fetch(weatherData)
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         var today = $('#currentDay');
         // city name
         var currentCity = $('#inputCity').val();
